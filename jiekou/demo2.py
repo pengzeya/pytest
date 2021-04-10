66287d4be5b36e1f2163b50eb3d7d02df6a6c9f5
pengzeya <281232686@qq.com>
2021-04-10 23:29:00 +0800

    接口学习

---
+++ jiekou/demo2.py
@@ -0,0 +1,14 @@
+# - * - coding : utf-8 - * -
+# @Time ： 2021/4/8 22:37 
+# @author ： pengzeya
+# @Email ： 281232686@qq.com
+list = [i for i in range(0,10,2)]
+print(list)
+
+a = [1,2,3,4,5]
+list = []
+for i in a:
+    list.append(i*i)
+print(list)
+
+a = [[“A”,'1'][“B”,'2']]
